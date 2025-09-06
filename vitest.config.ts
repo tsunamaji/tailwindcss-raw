d87c36d16ec0a684c04e559e16152bdde7178231
Rózsa Zoltán <67325669+rozsazoltan@users.noreply.github.com>
2025-09-06 12:24:52 +0200

    fix: use glob for array of vitest projects
    * https://github.com/vitest-dev/vitest/discussions/8535
    * https://stackoverflow.com/a/79757382/15167500

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,15 +1,10 @@
 import { defineConfig } from 'vitest/config'
-import fs from 'fs'
-import path from 'path'
-
-const packagesDir = path.resolve(__dirname, 'packages')
-
-const projects = fs.readdirSync(packagesDir, { withFileTypes: true })
-  .filter(dirent => dirent.isDirectory())
-  .map(dirent => path.join(packagesDir, dirent.name))
 
 export default defineConfig({
   test: {
-    projects,
+    projects: [
+      './packages/*',
+      '!./packages/*.*',
+    ],
   },
 })
